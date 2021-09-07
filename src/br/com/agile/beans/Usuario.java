3519be5eef800cd6b163f9fdbd8797f534b5ddfd
José Ramos <37668590+josersn@users.noreply.github.com>
2021-09-07 20:21:55 -0300

    add more variables to User Entity

--- src/br/com/agile/beans/Usuario.java
+++ src/br/com/agile/beans/Usuario.java
@@ -8,48 +8,79 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String senha;
-	
+	private String CPF;
+	private int numero;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
-	
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
-	public Usuario(String nome, String email, String senha) {
-		super();
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Usuario(String id, String nome, String email, String senha, String CPF, int numero) {
 		this.id = UUID.randomUUID().toString();
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
+		this.CPF = CPF;
+		this.numero = numero;
 	}
 
 	public Usuario() {
-		super();
 		this.id = UUID.randomUUID().toString();
 	}
 
-
+	@Override
 	public String toString() {
-		return "Usuario [nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+		return "Usuario{" +
+				"id='" + id + '\'' +
+				", nome='" + nome + '\'' +
+				", email='" + email + '\'' +
+				", senha='" + senha + '\'' +
+				", CPF='" + CPF + '\'' +
+				", numero=" + numero +
+				'}';
 	}
-	
-	
 }
