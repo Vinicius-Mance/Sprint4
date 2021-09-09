bd5ec2ee2ac6ef885c1f78983446ab123409e5e7
Vinicius-Mance <vinimanceprof@gmail.com>
2021-09-09 19:56:11 -0300

    página cadastro completa

--- WebContent/js/cadastro.js
+++ WebContent/js/cadastro.js
@@ -52,21 +52,136 @@ const validarCampoSenha = () => {
 
 }
 
-const temNumero = string => {
+let verSenha = document.getElementById('verSenha');
+let esconderSenha = document.getElementById('esconderSenha');
+verSenha.onclick = function () { mostrarSenha(); }
+esconderSenha.onclick = function () { mostrarSenha(); }
 
-  for( let i = 0; i < string.length; i++){
-    if(!isNaN(string.charAt(i)) && !(string.charAt(i) === " ") ){
+let visivel = false;
+const mostrarSenha = () => {
+
+  if (!visivel) {
+    verSenha.style.display = "block";
+    esconderSenha.style.display = "none";
+    campoSenha.type = "text";
+    visivel = true;
+  } else {
+    verSenha.style.display = "none";
+    esconderSenha.style.display = "block";
+    campoSenha.type = "password";
+    visivel = false;
+  }
+
+}
+
+let campoCpf = document.getElementById('campoCpf');
+let erroCpf = document.getElementById('erroCpf');
+
+campoCpf.onkeyup = function () { mascaraCpf(); }
+
+const mascaraCpf = () => {
+
+  erroCpf.innerHTML = "";
+
+  campoCpf.value = campoCpf.value
+  .replace(/\D+/g, '')
+  .replace(/(\d{3})(\d)/, '$1.$2')
+  .replace(/(\d{3})(\d)/, '$1.$2')
+  .replace(/(\d{3})(\d)/, '$1-$2')
+  .replace(/(-\d{2})\d+?$/, '$1');
+
+    if (!testarCpf(campoCpf)) {
+      erroCpf.innerHTML = "Insira um CPF válido";
+      return false;
+    } else {
       return true;
     }
+
   }
+
+const testarCpf = (varCampoCpf) => {
+  let soma = 0;
+  let resto;
+  let cpf = varCampoCpf.value.replace(/\D+/g, '');
+
+  if (!cpf ||
+      cpf.length != 11 ||
+      cpf == "00000000000" ||
+      cpf == "11111111111" ||
+      cpf == "22222222222" ||
+      cpf == "33333333333" ||
+      cpf == "44444444444" ||
+      cpf == "55555555555" ||
+      cpf == "66666666666" ||
+      cpf == "77777777777" ||
+      cpf == "88888888888" ||
+      cpf == "99999999999") {
+      return false;
+  }
+
+  for (var i = 1; i <= 9; i++) {
+    soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+  }
+
+  if (resto == 10 || resto == 11) {
+    resto = 0;
+  }
+
+  if (resto != parseInt(cpf.substring(9, 10))) {
     return false;
+  }
+
+  soma = 0;
+
+  for (var i = 1; i <= 10; i++) {
+    soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+  }
+
+  if (resto == 10 || resto == 11) {
+    resto = 0;
+  }
+
+  if (resto == parseInt(cpf.substring(10, 11))) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
+
+let campoTelefone = document.getElementById('campoTelefone');
+let erroTelefone = document.getElementById('erroTelefone');
+
+campoTelefone.onkeyup = function () {
+  mascaraTelefone();
+}
+
+const mascaraTelefone = () => {
+
+  erroTelefone.innerHTML = "";
+
+  campoTelefone.value = campoTelefone.value
+    .replace(/\D+/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(-\d{4})\d+?$/, '$1')
+
+    if (campoTelefone.value.replace(/\D+/g, '').length < 10) {
+      erroTelefone.innerHTML = "Insira um número válido";
+      return false;
+    } else {
+      return true;
+    }
+
+  }
 
 document.getElementById('formularioCadastro').onsubmit = function (e)
 {
   e.preventDefault();
-  console.clear();
-  console.log("campo nome: "+validarCampoNome());
-  console.log("campo email: "+validarCampoEmail());
-  console.log("campo senha: "+validarCampoSenha());
+    if (validarCampoNome() && validarCampoEmail() && validarCampoSenha() && mascaraCpf() && mascaraTelefone()) {
+      document.getElementById('formularioCadastro').submit();
+    }
  }
